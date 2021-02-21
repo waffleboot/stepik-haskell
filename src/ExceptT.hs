c7fe8514aa0da0477c8bc6d4ad8d01a37157e093
Andrei Yangabishev <waffleboot@gmail.com>
2021-02-22 00:09:15 +0300

    applicative functor

--- src/ExceptT.hs
+++ src/ExceptT.hs
@@ -17,3 +17,21 @@ instance Functor (Except e) where
 instance Functor m => Functor (ExceptT e m) where
     fmap :: (a -> b) -> ExceptT e m a -> ExceptT e m b
     fmap f = ExceptT . fmap (fmap f) . runExceptT
+
+instance Applicative (Except e) where
+    pure :: a -> Except e a
+    pure = Except . Right
+    (<*>) :: Except e (a -> b) -> Except e a -> Except e b
+    f <*> v = Except $ (runExcept f) <*> (runExcept v)
+    -- f <*> v = Except $ updater (runExcept f) (runExcept v) where
+    --     updater (Left e) _  = Left e
+    --     updater (Right g) x = fmap g x
+
+instance Applicative m => Applicative (ExceptT e m) where
+    pure :: a -> ExceptT e m a
+    pure = ExceptT . pure . Right
+    (<*>) :: ExceptT e m (a -> b) -> ExceptT e m a -> ExceptT e m b
+    f <*> v = ExceptT $ liftA2 (<*>) (runExceptT f) (runExceptT v)
+    -- f <*> v = ExceptT $ liftA2 updater (runExceptT f) (runExceptT v) where
+    --     updater (Left e) _  = Left e
+    --     updater (Right g) x = fmap g x
