1b4509bcb3ae5b606454ba840051bf9662b0303d
Andrei Yangabishev <waffleboot@gmail.com>
2021-02-22 03:39:15 +0300

    tile map lesson

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,48 @@
+import ExceptT
+
 main :: IO ()
 main = putStrLn "Test suite not yet implemented"
+
+data Tile = Floor | Chasm | Snake deriving Show
+
+data DeathReason = Fallen | Poisoned deriving (Eq, Show)
+
+type Point = (Integer, Integer)
+
+type GameMap = Point -> Tile
+
+moves :: GameMap -> Int -> Point -> [Either DeathReason Point]
+moves map k = runExceptT . moves' k where
+    moves' 0 p       = verify p
+    moves' k p@(x,y) = do
+        verify p
+        n <- ExceptT $ Right <$> [(x+1,y),(x-1,y),(x,y-1),(x,y+1)]
+        moves' (k-1) n
+    verify p = do
+        case map p of
+            Floor -> return p
+            Chasm -> (ExceptT . return . Left) Fallen
+            Snake -> (ExceptT . return . Left) Poisoned
+
+waysToDie :: DeathReason -> GameMap -> Int -> Point -> Int
+waysToDie r map steps p = length (filter ( == (Left r)) (moves map steps p))
+
+map1 :: GameMap
+map1 (2, 2) = Snake
+map1 (4, 1) = Snake
+map1 (x, y)
+  | 0 < x && x < 5 && 0 < y && y < 5 = Floor
+  | otherwise                        = Chasm
+
+foo :: Int -> Int -> ExceptT Int [] Int
+foo 0 p = verify p
+foo count p = verify p >> ExceptT (next p) >>= foo (count-1)
+
+next :: Int -> [Either Int Int]
+next p = fmap Right [p-1,p+1]
+
+verify :: Int -> ExceptT Int [] Int
+verify p = do
+    if p `mod` 3 == 0
+    then ExceptT [Left p]
+    else return p
